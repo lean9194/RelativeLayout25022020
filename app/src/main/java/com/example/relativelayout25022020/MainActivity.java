00cb6e5f0a022dd1d89e7e13368e6e888a705d1c
git-test <lean9194@gmail.com>
2020-03-15 09:33:57 +0700

    first commit

---
+++ app/src/main/java/com/example/relativelayout25022020/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.relativelayout25022020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    View mtvTiltle;
+    Button mBtnShow;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        mBtnShow = findViewById(R.id.btnButtonShow);
+//        mtvTiltle = findViewById(R.id.tvTitle1);
+//        Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
+//        int a = 5;
+//        a--;
+//        Log.d("BBB",String.valueOf(a));
+//        mBtnShow.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//        Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
